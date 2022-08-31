22d2ecc76d1d00ba78e08c224e519d704d74e268
David Faletto <davidfaletto@gmail.com>
2022-08-31 19:57:37 -0300

    incorporando sweet alert al proyecto

--- js/botones.js
+++ js/botones.js
@@ -20,13 +20,11 @@ const botonesCatalogo = () =>{
             localStorage.setItem("cantidad",sumadorCarrito);
 
             actualizarCantidad(localStorage.getItem("cantidad"));
+            notificacion();
             })
          }
         );
 }
-
-
-
 
 
 const botonesCarrito = () =>{
@@ -54,9 +52,11 @@ const vaciarCarrito = () =>{
     carrito.length = 0;
     sumadorCarrito = 0;
     mostrarCarrito();
+    notificacionVaciar();
     localStorage.setItem("cantidad", sumadorCarrito);
     actualizarCantidad(localStorage.getItem("cantidad"));
     localStorage.removeItem("carrito");
+    
   })
 }
 
@@ -65,3 +65,32 @@ const actualizarCantidad = (cantidad) =>{
   mostrarNumero.innerText = cantidad;
 }
 
+const notificacion = () =>{
+  Swal.fire({
+    position: 'top-end',
+    icon: 'success',
+    title: 'Producto agregado al carrito',
+    showConfirmButton: false,
+    timer: 500
+  })
+}
+
+const notificacionVaciar = () =>{
+  Swal.fire({
+    title: 'Desea Eliminar todos los productos?',
+    text: "Estas apunto de quitar todos los productos que agregaste!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Si, Eliminar todo!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'Carrito vacio!',
+        'Sigue buscando, algo te va a gustar',
+        'success'
+      )
+    }
+  })
+}
